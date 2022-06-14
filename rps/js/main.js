4c7a65d226030abfaeb62982a7037db6121cb589
Laura <moomal.abro@gmail.com>
2022-06-14 17:42:27 +0500

    styling

--- rps/js/main.js
+++ rps/js/main.js
@@ -1,1 +1,11 @@
+let options = document.getElementsByTagName('figure')
+    for (i=0; i < options.length; i++) {
+        options[i].addEventListener("mouseover", (e) => e.currentTarget.style.color = "#000");
+        options[i].addEventListener("mouseout", (e) => e.currentTarget.style.color = "#aaa");
+        options[i].addEventListener("click", result);
+    }
 
+function result() {
+    
+}
+//e.target.style.color ="black"
