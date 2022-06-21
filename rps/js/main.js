4e21ceee206b0f391b9ab528377e35e6c975bfa7
Laura <moomal.abro@gmail.com>
2022-06-21 07:57:57 +0500

    rock paper scissors done!

--- rps/js/main.js
+++ rps/js/main.js
@@ -1,12 +1,39 @@
-let options = document.getElementsByTagName('figure')
-    for (i=0; i < options.length; i++) {
-        options[i].addEventListener("mouseover", (e) => e.currentTarget.style.color = "#000");
-        options[i].addEventListener("mouseout", (e) => e.currentTarget.style.color = "#aaa");
-        options[i].addEventListener("click", result);
-    }
+let options = document.querySelectorAll('#options figure');
+options.forEach(opt => {
+   opt.addEventListener("mouseover", (e) => e.currentTarget.style.color = "#000");
+   opt.addEventListener("mouseout", (e) => e.currentTarget.style.color = "#aaa");
+   opt.addEventListener("click", result, { once: true });
+});
 
 function result(e) {
-    let chosen = e.currentTarget.cloneNode(true);
-    document.getElementById("options").appendChild(chosen);
+   let chosen = e.currentTarget;
+   let sibling = chosen.parentNode.parentNode.setAttribute("id", "done");
+   document.querySelector("#result").setAttribute("class", "display");
+   document.querySelector("#player").appendChild(chosen.cloneNode(true));
+   let aiChoice = aiChoose();
+   document.querySelector("#ai").appendChild(aiChoice);
+   const WINNER = {
+      'rock': 'paper',
+      'paper': 'scissors',
+      'scissors': 'rock'
+   }
+   let winnerText = ""
+   if (aiChoice.className === e.currentTarget.className) {
+      winnerText = "It was a tie!"
+   } else if (aiChoice.className === WINNER[e.currentTarget.className]) {
+      winnerText = "I won!"
+   } else {
+      winnerText = "You won!"
+   }
+   document.querySelector("#winner h3").innerText = winnerText;
 }
-//e.target.style.color ="black"
+
+function aiChoose() {
+   const CHOICES = {
+      1: "rock",
+      2: "paper",
+      3: "scissors"
+   }
+   let choiceNum = Math.floor(Math.random() * 3) + 1;
+   return document.getElementsByClassName(CHOICES[choiceNum])[0].cloneNode(true);
+}
